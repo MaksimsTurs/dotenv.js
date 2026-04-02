3d7617ac815e61c5be39a92d060c689166100380
MaksimsTurs <maksims07turs@gmail.com>
2026-04-03 00:20:45 +0200

    feat: add support of multiline string

--- src/dotenv.js
+++ src/dotenv.js
@@ -6,7 +6,7 @@ const DEFAULT_PATH = `${process.cwd()}/.env`;
  *  @param {string} [path] path to the .env file, otherwise `${process.cwd()}/.env`.
  *  @returns {void}
  */
-function dotenv(path = DEFAULT_PATH) {
+export default function dotenv(path = DEFAULT_PATH) {
   const buffer = readFileSync(path, { encoding: "ascii" });
   const lines = buffer.split(/\n/);
 
@@ -23,18 +23,30 @@ function dotenv(path = DEFAULT_PATH) {
       }
 
       const keyEndIndex = line.indexOf("=");
+      const valueStartIndex = keyEndIndex + 1;
       const key = getKey(keyEndIndex, line);
 
       if(isEmptyStr(key)) {
         console.error(`[dotenv]: Variable on line ${index} has no key!`);
       } else {
-        const value = getValue(keyEndIndex + 1, line);
-        process.env[key] = value;
+        let value = "";
+
+        if(isMultilineString(valueStartIndex, line)) {
+          const stringDelimiter = getStringDelimiter(valueStartIndex, line);
+          const indexRef = { index: index + 1 };
+
+          value = getValue(valueStartIndex, line);
+          value += collectMultilineString(indexRef, lines, stringDelimiter);
+          index = indexRef.index;
+        } else {
+          value = getValue(valueStartIndex, line);
+        }
+
+        process.env[key] = formatString(value);
       }
     }
   }
 };
-
 /**
  *  @param {number} end
  *  @param {string} line
@@ -43,7 +55,6 @@ function dotenv(path = DEFAULT_PATH) {
 function getKey(end, line) {
   return line.slice(0, end);
 };
-
 /**
  *  @param {number} start
  *  @param {string} line
@@ -52,7 +63,44 @@ function getKey(end, line) {
 function getValue(start, line) {
   return line.slice(start, line.length);
 };
+/**
+ *  @param {number} start
+ *  @param {string} line
+ *  @returns {string}
+ */
+function getStringDelimiter(start, line) {
+  return line[start];
+};
+/**
+ *  @param {{ index: number }} indexRef
+ *  @param {string[]} lines
+ *  @param {string} delimiter
+ *  @returns {void}
+ */
+function collectMultilineString(indexRef, lines, delimiter) {
+  let string = "";
 
+  for(let index = indexRef.index; index < lines.length; index++) {
+    const indexOfDelimiter = lines[index].indexOf(delimiter);
+
+    if(indexOfDelimiter >= 0) {
+      indexRef.index = index;
+      string += lines[index];
+      return string;
+    } else {
+      string += lines[index];
+    }
+  }
+
+  console.log(`[dotenv]: Multiline string with start on line ${indexRef.index} is not closed with ${delimiter}!`);
+};
+/**
+ *  @param {string} value
+ *  @returns {string}
+ */
+function formatString(value) {
+  return value.replace(/^['"`](.+)['"`]$/, "$1");
+};
 /**
  *  @param {string} line
  *  @returns {boolean}
@@ -60,7 +108,6 @@ function getValue(start, line) {
 function isComment(line) {
   return line[0] === "#";
 };
-
 /**
  *  @param {string} line
  *  @returns {boolean}
@@ -68,5 +115,17 @@ function isComment(line) {
 function isEmptyStr(line) {
   return line.length === 0;
 };
-
-dotenv();
+/**
+ *  @param {number} start
+ *  @param {string} line
+ *  @returns {boolean}
+ */
+function isMultilineString(start, line) {
+  return((line[start] === "\"" ||
+          line[start] === "'" ||
+          line[start] === "`") &&
+         (line[line.length - 1] != "\"" &&
+          line[line.length - 1] != "'" &&
+          line[line.length - 1] != "`")
+  );
+};
