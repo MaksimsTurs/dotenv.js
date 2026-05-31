afffaac05914d7aaf6ad8a7fdbe509f9899dcdf1
MaksimsTurs <maksims07turs@gmail.com>
2026-05-31 15:45:33 +0200

    feat: replace if/else statements with simple state machine

--- src/dotenv.js
+++ src/dotenv.js
@@ -1,127 +1,148 @@
 import { readFileSync } from "node:fs";
 
 const DEFAULT_PATH = `${process.cwd()}/.env`;
+const PARSE_STEPS = {
+  NONE: -1,
+  KEY:   0,
+  EQUAL: 1,
+  VALUE: 2
+};
+const STATE = {
+  step: PARSE_STEPS.KEY,
+  text: "",
+  col: 0,
+};
 
 /**
  *  @param {string} [path] path to the .env file, otherwise `${process.cwd()}/.env`.
  *  @returns {void}
  */
 export default function dotenv(path = DEFAULT_PATH) {
-  const buffer = readFileSync(path, { encoding: "ascii" });
-  const lines = buffer.split(/\n/);
+  let key = "";
+  let value = "";
+  
+  try {
+    STATE.text = readFileSync(path, { encoding: "ascii" });
+  } catch(error) {
+    console.error(`[dotenv.js]: Error while reading the file ${path} is occur.`)
+    console.error(`[dotenv.js]: ${error.message}`);
+  }
 
-  for(let index = 0; index < lines.length; index++) {
-    const line = lines[index];
-
-    // A=V smallest possible valid env variable. 
-
-    if(line.length < 3 && !isEmptyStr(line)) {
-      console.error(`[dotenv]: Variable on line ${index} has either no value or key!`);
-    } else if(!isEmptyStr(line) && !isComment(line)) {
-      const keyEndIndex = line.indexOf("=");
-      const valueStartIndex = keyEndIndex + 1;
-      const key = getKey(keyEndIndex, line);
-
-      if(isEmptyStr(key)) {
-        console.error(`[dotenv]: Variable on line ${index} has no key!`);
-      } else {
-        let value = "";
-
-        if(isMultilineString(valueStartIndex, line)) {
-          const stringDelimiter = getStringDelimiter(valueStartIndex, line);
-          const indexRef = { index: index + 1 };
-
-          value = getValue(valueStartIndex, line);
-          value += collectMultilineString(indexRef, lines, stringDelimiter);
-          index = indexRef.index;
-        } else {
-          value = getValue(valueStartIndex, line);
-        }
-
-        process.env[key] = formatString(value);
-      }
+  while(!isEOF()) {
+    switch(STATE.step) {
+      case PARSE_STEPS.KEY:
+        key = parseKey();
+        // Skip equal character
+        STATE.col++;
+        STATE.step = PARSE_STEPS.VALUE;
+      break;
+      case PARSE_STEPS.VALUE:
+        value = parseValue();
+        skipSpecialCharacters();
+        process.env[key] = value;
+        
+        key = "";
+        value = "";
+        
+        STATE.step = PARSE_STEPS.KEY;
+      break;
     }
   }
 };
 /**
- *  @param {number} end
- *  @param {string} line
  *  @returns {string}
  */
-function getKey(end, line) {
-  return line.slice(0, end);
+function parseKey() {
+  let key = "";
+
+  while(!isEndOfKey()) {
+    key += STATE.text[STATE.col++];
+  }
+
+  return key;
 };
 /**
- *  @param {number} start
- *  @param {string} line
  *  @returns {string}
  */
-function getValue(start, line) {
-  return line.slice(start, line.length);
+function parseValue() {
+  if(isString()) {
+    return parseString();
+  } else {
+    return parseAny();
+  }
 };
 /**
- *  @param {number} start
- *  @param {string} line
  *  @returns {string}
  */
-function getStringDelimiter(start, line) {
-  return line[start];
+function parseString() {
+  let value = ""
+
+  STATE.col++
+
+  while(!isEndOfString()) {
+    value += STATE.text[STATE.col++]
+  }
+  
+  STATE.col++
+
+  return value
+}
+/**
+ *  @returns {string}
+ */
+function parseAny() {
+  let value = ""
+
+  while(!isEndOfAny()) {
+    value += STATE.text[STATE.col++]
+  }
+
+  return value
 };
 /**
- *  @param {{ index: number }} indexRef
- *  @param {string[]} lines
- *  @param {string} delimiter
  *  @returns {void}
  */
-function collectMultilineString(indexRef, lines, delimiter) {
-  let string = "";
-
-  for(let index = indexRef.index; index < lines.length; index++) {
-    const indexOfDelimiter = lines[index].indexOf(delimiter);
-
-    if(indexOfDelimiter >= 0) {
-      indexRef.index = index;
-      string += lines[index];
-      return string;
-    } else {
-      string += lines[index];
-    }
+function skipSpecialCharacters() {
+  while(STATE.col < STATE.text.length && isSpecialCharacter()) {
+    STATE.col++
   }
-
-  console.log(`[dotenv]: Multiline string with start on line ${indexRef.index} is not closed with ${delimiter}!`);
 };
 /**
- *  @param {string} value
- *  @returns {string}
- */
-function formatString(value) {
-  return value.replace(/^['"`](.+)['"`]$/, "$1");
-};
-/**
- *  @param {string} line
  *  @returns {boolean}
  */
-function isComment(line) {
-  return line[0] === "#";
-};
-/**
- *  @param {string} line
- *  @returns {boolean}
- */
-function isEmptyStr(line) {
-  return line.length === 0;
-};
-/**
- *  @param {number} start
- *  @param {string} line
- *  @returns {boolean}
- */
-function isMultilineString(start, line) {
-  return((line[start] === "\"" ||
-          line[start] === "'" ||
-          line[start] === "`") &&
-         (line[line.length - 1] != "\"" &&
-          line[line.length - 1] != "'" &&
-          line[line.length - 1] != "`")
+function isSpecialCharacter() {
+  return(
+    STATE.text[STATE.col] === "\n" ||
+    STATE.text[STATE.col] === "\r"
   );
+};
+/**
+ *  @returns {boolean}
+ */
+function isEOF() {
+  return STATE.col > STATE.text.length
+}
+/**
+ *  @returns {boolean}
+ */
+function isEndOfString() {
+  return STATE.col >= STATE.text.length || STATE.text[STATE.col] === "`";
+};
+/**
+ *  @returns {boolean}
+ */
+function isEndOfAny() {
+  return STATE.col >= STATE.text.length || STATE.text[STATE.col] === "\n";
+};
+/**
+ *  @returns {boolean}
+ */
+function isEndOfKey() {
+  return STATE.col >= STATE.text.length || STATE.text[STATE.col] === "=";
+};
+/**
+ *  @returns {boolean}
+ */
+function isString() {
+  return STATE.text[STATE.col] === "`";
 };
